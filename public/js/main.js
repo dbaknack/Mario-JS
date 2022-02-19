631527203630b7ee7ff970cb8fe4790ab3a365b8
abe hernandez <dbaknack@gmail.com>
2022-02-19 15:51:49 -0700

    loadLevel function works now with objects defined in tiles.json

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,29 @@
 import SpriteSheet from './SpriteSheet.js';
 import {loadImage,loadLevel} from './loaders.js';
 
-
-function drawBackground(backgrounds,context,sprites) {
-    backgrounds.ranges.forEach(([x1,x2,y1,y2]) => {
-        for(let x = x1; x < x2; ++x) {
-            for(let y = y1; y < y2; ++y) {
-                sprites.drawTile(backgrounds.tile,context, x, y);
+function drawBackground(background,context,sprites){
+    background.ranges.forEach(([x1,x2,y1,y2]) => {
+        for(let x = x1; x < x2; ++x){
+            for(let y = y1; y < y2; ++y){
+                sprites.drawTile(background.tile, context,x,y);
             }
-        }
+        }    
     });
 }
+
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 loadImage('./img/tiles.png')
-    .then(image => {
-        const sprites = new SpriteSheet(image,16,16)
-        sprites.define('ground',0,0);
-        sprites.define('sky',3,23)
+.then(image => {
+    const sprites = new SpriteSheet(image,16,16);
+    sprites.define('ground',0,0);
+    sprites.define('sky',3,23);
 
     loadLevel('1-1')
-        .then(level => {
-        drawBackground(level.backgrounds[0],context,sprites);
+    .then(level =>{
+        level.backgrounds.forEach(background => {
+            drawBackground(background , context, sprites);
+        });
     });
-
-
-        for(let x = 0; x < 25; ++x) {
-            for(let y = 12; y < 14; ++y) {
-                sprites.drawTile('ground',context, x, y)
-            }
-        }
-    });
+});
