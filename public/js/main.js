407f05eb771b48cc64e37150b421eea16d25f423
abe hernandez <dbaknack@gmail.com>
2022-02-23 08:19:18 -0700

    modification of keyboard state, set space bar to some other value

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,8 @@ import {loadLevel} from './loaders.js';
 import {createMario} from './entities.js';
 import {loadBackgroundSprites} from './sprites.js';
 import {createBackgroundLayer, createSpriteLayer} from './layers.js';
+import Keyboard from './KeyboardState.js';
+
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
@@ -19,9 +21,20 @@ Promise.all([
     const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
     comp.layers.push(backgroundLayer);
 
-    const gravity = 30;
+    const gravity = 2000;
     mario.pos.set(64, 180);
-    mario.vel.set(200, -600);
+
+
+    const SPACE = 32;
+    const input = new Keyboard();
+    input.addMapping(SPACE, keyState => {
+        if (keyState) {
+            mario.jump.start();
+        } else {
+            mario.jump.cancel();
+        }
+    });
+    input.listenTo(window);
 
 
     const spriteLayer = createSpriteLayer(mario);
@@ -29,9 +42,11 @@ Promise.all([
 
     const timer = new Timer(1/60);
     timer.update = function update(deltaTime) {
-        comp.draw(context);
         mario.update(deltaTime);
-        mario.vel.y += gravity;
+
+        comp.draw(context);
+
+        mario.vel.y += gravity * deltaTime;
     }
 
     timer.start();
